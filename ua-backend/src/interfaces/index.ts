721151501732ddc8ed4afe27f13c76d9c58a7a36
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-06 07:48:40 +0100

    make interfaces shared

---
+++ ua-backend/src/interfaces/index.ts
@@ -0,0 +1,3 @@
+export { ILogin } from './login.interface';
+export { ISignup } from './signup.interface';
+export { IUser } from './user.interface';
---
+++ ua-backend/src/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+  readonly username: string;
+  readonly email: string;
+  readonly isAdmin: boolean;
+}
