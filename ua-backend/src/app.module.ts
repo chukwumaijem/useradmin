4a88319ddb899402a80980f0e6ebc2ada4a2a69a
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-06 07:31:56 +0100

    scafold user module

--- ua-backend/src/app.module.ts
+++ ua-backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 console.info(process.env.MONGODB_URI);
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), AuthModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), AuthModule, UserModule],
 })
 
 export class AppModule { }
